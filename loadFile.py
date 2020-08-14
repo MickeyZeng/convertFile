539d3a1df071dbd45c3949b8c420a290364414ca
Jianqi <a1752399@student.adelaide.edu.au>
2020-08-15 01:03:29 +0930

    Add a new function

--- loadFile.py
+++ loadFile.py
@@ -1,12 +1,21 @@
-
 import numpy as np
 from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
-    np.set_printoptions(threshold=np.inf)
-    test = np.load("testDic/IM-0004-0002.png.npy")
-    for i in test:
-        for j in i:
-            if j == 1:
-               print(i.tolist().index(j))
-       # print(i)
+    # np.set_printoptions(threshold=np.inf)
+    test = np.load("testDic/26_IM-0003-0002.png.npy")
+    # temp = np.zeros(test.shape)
+    # groundTruth = 0
+    # if groundTruth == 0:
+    #     result = np.stack((test, temp))
+    # else:
+    #     result = np.stack((temp, test))
+    #
+    # result = result.astype('uint8')
+    # np.save("testDic/9_IM-0001-0002.png.npy", result)
+
+    # for display the scribble and original image
+    image = plt.imread("testDic/IM-0003-0002.png")
+    plt.imshow(image)
+    plt.imshow(test, alpha=.5)
+    plt.show()
