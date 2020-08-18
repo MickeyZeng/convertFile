20bb084338d13bd4e64cf706f52f76f950482c0c
Jianqi <a1752399@student.adelaide.edu.au>
2020-08-18 23:55:36 +0930

    Update some code

--- loadFile.py
+++ loadFile.py
@@ -1,21 +1,132 @@
+# import numpy as np
+# from matplotlib import pyplot as plt
+#
+# if __name__ == '__main__':
+#     # np.set_printoptions(threshold=np.inf)
+#     # test = np.load("testDic/279_IM-0004-0001.png.npy")
+#     # temp = np.zeros(test.shape)
+#     # groundTruth = 0
+#     # if groundTruth == 0:
+#     #     result = np.stack((test, temp))
+#     # else:
+#     #     result = np.stack((temp, test))
+#     #
+#     # result = result.astype('uint8')
+#     # np.save("testDic/9_IM-0001-0002.png.npy", result)
+#
+#     # for display the scribble and original image
+#     # image = plt.imread("testDic/IM-0003-0002.png")
+#     # plt.imshow(image)
+#     # plt.imshow(test, alpha=.5)
+#     # plt.show()
+#
+
+import os
+
 import numpy as np
-from matplotlib import pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+
+
+def copy_files_to_new_csv(files):
+    tempCSV = pd.read_csv("validate.csv")
+    tempPath = []
+    result_rows = None
+
+    for i in files:
+        temp = i.split("/")[6] + '/' + i.split("/")[7]
+        temp = temp.split('.')[0] + '.png'
+        tempPath.append(temp)
+
+    for i in tempPath:
+        if result_rows is not None:
+            result_rows = pd.concat([tempCSV.loc[tempCSV['image_path'] == i], result_rows], ignore_index=True)
+        else:
+            result_rows = tempCSV.loc[tempCSV['image_path'] == i]
+    result_rows.to_csv("availableTemp.csv", index=False)
+    pass
+
+
+def findUnlabeledImage(file):
+    test = np.load(file)
+
+    # if all 0 - did not scribble yet
+    if test.max() == 0:
+        return file
+    # else did scribble
+
+    return
+
+
+def main():
+    root_folder_name = '/testScribble'
+    root_path = os.getcwd() + root_folder_name
+    # 1: get all the folders
+    folders = os.listdir(root_path)
+    # print(dirs)
+    not_label_arr = []
+
+    # 2: loop every folder get all the files within the folders
+    for folder in tqdm(folders):
+        if folder == '.DS_Store':
+            continue
+        path_to_file = root_path + '/' + folder + '/'
+        folder_files = os.listdir(path_to_file)
+        for file in folder_files:
+            if file == '.DS_Store':
+                continue
+            file_full_path = path_to_file + file
+
+            # 3: rename the file
+            # if file == '.DS_Store':
+            #     continue
+            # splitted_name = file.split('_')
+            # new_name = splitted_name[1]
+            # # print(new_name)
+            # os.rename(file_full_path, path_to_file + new_name)
+
+            # 4: swap axies
+
+            # 5: change 2 to -1, change 1 to 2, -1 to 1
+
+            # 6: change thickness
+
+            result = findUnlabeledImage(file_full_path)
+            if result:
+                not_label_arr.append(result)
+            # old code
+            # test = np.load(file_full_path)
+            #
+            # # 1 -> neg, 2 -> pos
+            # test = np.where(test == 1, 3, test)
+            # test = np.where(test == 2, 1, test)
+            # test = np.where(test == 3, 2, test)
+            #
+            # # chagne axes
+            # test = np.swapaxes(test, 0, 1)
+            # test = np.swapaxes(test, 1, 2)
+            #
+            # np.save(file_full_path, test)
+    print(len(not_label_arr))
+    copy_files_to_new_csv(not_label_arr)
+
+
+def main1():
+    test = np.load(
+        "/Users/mickey/PycharmProjects/loadNumpy/testScribble/9/IM-0001-0002.npy")
+
+    # # 1 -> neg, 2 -> pos
+    # test = np.where(test == 1, 3, test)
+    # test = np.where(test == 2, 1, test)
+    # test = np.where(test == 3, 2, test)
+
+    # # chagne axes
+    # test = np.swapaxes(test, 0, 1)
+    # test = np.swapaxes(test, 1, 2)
+
+    pass
+
 
 if __name__ == '__main__':
-    # np.set_printoptions(threshold=np.inf)
-    test = np.load("testDic/279_IM-0004-0001.png.npy")
-    # temp = np.zeros(test.shape)
-    # groundTruth = 0
-    # if groundTruth == 0:
-    #     result = np.stack((test, temp))
-    # else:
-    #     result = np.stack((temp, test))
-    #
-    # result = result.astype('uint8')
-    # np.save("testDic/9_IM-0001-0002.png.npy", result)
-
-    # for display the scribble and original image
-    image = plt.imread("testDic/IM-0003-0002.png")
-    plt.imshow(image)
-    plt.imshow(test, alpha=.5)
-    plt.show()
+    # print('hello')
+    main()
