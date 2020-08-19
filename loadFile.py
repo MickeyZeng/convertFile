97a7c627b6d2bf1b06c76debb817997cee2ad913
Jianqi <a1752399@student.adelaide.edu.au>
2020-08-19 23:18:12 +0930

    add a script

--- loadFile.py
+++ loadFile.py
@@ -3,7 +3,9 @@
 #
 # if __name__ == '__main__':
 #     # np.set_printoptions(threshold=np.inf)
-#     # test = np.load("testDic/279_IM-0004-0001.png.npy")
+#     test = np.load("testDic/210_IM-0001-0001.png.npy")
+#     test = np.swapaxes(test, 1, 2)
+#     test = np.swapaxes(test, 0, 1)
 #     # temp = np.zeros(test.shape)
 #     # groundTruth = 0
 #     # if groundTruth == 0:
@@ -17,9 +19,8 @@
 #     # for display the scribble and original image
 #     # image = plt.imread("testDic/IM-0003-0002.png")
 #     # plt.imshow(image)
-#     # plt.imshow(test, alpha=.5)
-#     # plt.show()
-#
+#     plt.imshow(test, alpha=.5)
+#     plt.show()
 
 import os
 
@@ -28,6 +29,8 @@ import pandas as pd
 from tqdm import tqdm
 
 
+#
+#
 def copy_files_to_new_csv(files):
     tempCSV = pd.read_csv("validate.csv")
     tempPath = []
@@ -58,6 +61,7 @@ def findUnlabeledImage(file):
     return
 
 
+#
 def main():
     root_folder_name = '/testScribble'
     root_path = os.getcwd() + root_folder_name
@@ -108,25 +112,54 @@ def main():
             #
             # np.save(file_full_path, test)
     print(len(not_label_arr))
-    copy_files_to_new_csv(not_label_arr)
+    print(not_label_arr)
+    # copy_files_to_new_csv(not_label_arr)
 
 
-def main1():
-    test = np.load(
-        "/Users/mickey/PycharmProjects/loadNumpy/testScribble/9/IM-0001-0002.npy")
-
-    # # 1 -> neg, 2 -> pos
-    # test = np.where(test == 1, 3, test)
-    # test = np.where(test == 2, 1, test)
-    # test = np.where(test == 3, 2, test)
-
-    # # chagne axes
-    # test = np.swapaxes(test, 0, 1)
-    # test = np.swapaxes(test, 1, 2)
-
-    pass
-
-
+#
+#
+# def main1():
+#     test = np.load(
+#         "/Users/mickey/PycharmProjects/loadNumpy/testScribble/9/IM-0001-0002.npy")
+#
+#     # # 1 -> neg, 2 -> pos
+#     # test = np.where(test == 1, 3, test)
+#     # test = np.where(test == 2, 1, test)
+#     # test = np.where(test == 3, 2, test)
+#
+#     # # chagne axes
+#     # test = np.swapaxes(test, 0, 1)
+#     # test = np.swapaxes(test, 1, 2)
+#
+#     pass
+#
+#
+# def main2():
+#     folder_name = 'truthScribble'
+#     result_folder = 'testScribble'
+#     files = os.listdir(folder_name)
+#     for file in files:
+#         if file == '.DS_Store':
+#             continue
+#
+#         content = np.load(folder_name + "/" + file)
+#
+#         # print(file)
+#         patientId = file.split("_")[0]
+#         newName = file.split("_")[1].split(".")[0] + "." + file.split("_")[1].split(".")[2]
+#         # os.rename(os.path.join(folder_name + '/' + file), newName)
+#
+#         # print(os.path.join(folder_name + '/' + file))
+#         # print(newName)
+#         np.save(result_folder + "/" + patientId + "/" + newName, content)
+#
+#
+# def main3():
+#     testPath = "testScribble/210/IM-0001-0001.png.npy"
+#     testContent = np.load(testPath)
+#     print(testContent.shape)
+#
+#
 if __name__ == '__main__':
     # print('hello')
     main()
