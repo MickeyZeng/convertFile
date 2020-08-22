4af29b2c5d6428f07cb72d951da5daa9f8daa51b
Jianqi <a1752399@student.adelaide.edu.au>
2020-08-22 14:37:43 +0930

    Add function to read the numpy

--- loadFile.py
+++ loadFile.py
@@ -26,7 +26,6 @@ import os
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
 
 #
@@ -61,59 +60,58 @@ def findUnlabeledImage(file):
     return
 
 
+# def main():
+#     root_folder_name = '/testScribble'
+#     root_path = os.getcwd() + root_folder_name
+#     # 1: get all the folders
+#     folders = os.listdir(root_path)
+#     # print(dirs)
+#     not_label_arr = []
 #
-def main():
-    root_folder_name = '/testScribble'
-    root_path = os.getcwd() + root_folder_name
-    # 1: get all the folders
-    folders = os.listdir(root_path)
-    # print(dirs)
-    not_label_arr = []
-
-    # 2: loop every folder get all the files within the folders
-    for folder in tqdm(folders):
-        if folder == '.DS_Store':
-            continue
-        path_to_file = root_path + '/' + folder + '/'
-        folder_files = os.listdir(path_to_file)
-        for file in folder_files:
-            if file == '.DS_Store':
-                continue
-            file_full_path = path_to_file + file
-
-            # 3: rename the file
-            # if file == '.DS_Store':
-            #     continue
-            # splitted_name = file.split('_')
-            # new_name = splitted_name[1]
-            # # print(new_name)
-            # os.rename(file_full_path, path_to_file + new_name)
-
-            # 4: swap axies
-
-            # 5: change 2 to -1, change 1 to 2, -1 to 1
-
-            # 6: change thickness
-
-            result = findUnlabeledImage(file_full_path)
-            if result:
-                not_label_arr.append(result)
-            # old code
-            # test = np.load(file_full_path)
-            #
-            # # 1 -> neg, 2 -> pos
-            # test = np.where(test == 1, 3, test)
-            # test = np.where(test == 2, 1, test)
-            # test = np.where(test == 3, 2, test)
-            #
-            # # chagne axes
-            # test = np.swapaxes(test, 0, 1)
-            # test = np.swapaxes(test, 1, 2)
-            #
-            # np.save(file_full_path, test)
-    print(len(not_label_arr))
-    print(not_label_arr)
-    # copy_files_to_new_csv(not_label_arr)
+#     # 2: loop every folder get all the files within the folders
+#     for folder in tqdm(folders):
+#         if folder == '.DS_Store':
+#             continue
+#         path_to_file = root_path + '/' + folder + '/'
+#         folder_files = os.listdir(path_to_file)
+#         for file in folder_files:
+#             if file == '.DS_Store':
+#                 continue
+#             file_full_path = path_to_file + file
+#
+#             # 3: rename the file
+#             # if file == '.DS_Store':
+#             #     continue
+#             # splitted_name = file.split('_')
+#             # new_name = splitted_name[1]
+#             # # print(new_name)
+#             # os.rename(file_full_path, path_to_file + new_name)
+#
+#             # 4: swap axies
+#
+#             # 5: change 2 to -1, change 1 to 2, -1 to 1
+#
+#             # 6: change thickness
+#
+#             result = findUnlabeledImage(file_full_path)
+#             if not (result):
+#                 not_label_arr.append(result)
+#             # old code
+#             # test = np.load(file_full_path)
+#             #
+#             # # 1 -> neg, 2 -> pos
+#             # test = np.where(test == 1, 3, test)
+#             # test = np.where(test == 2, 1, test)
+#             # test = np.where(test == 3, 2, test)
+#             #
+#             # # chagne axes
+#             # test = np.swapaxes(test, 0, 1)
+#             # test = np.swapaxes(test, 1, 2)
+#             #
+#             # np.save(file_full_path, test)
+#     print(len(not_label_arr))
+#     print(not_label_arr)
+#     # copy_files_to_new_csv(not_label_arr)
 
 
 #
@@ -134,32 +132,34 @@ def main():
 #     pass
 #
 #
-# def main2():
-#     folder_name = 'truthScribble'
-#     result_folder = 'testScribble'
-#     files = os.listdir(folder_name)
-#     for file in files:
-#         if file == '.DS_Store':
-#             continue
+def main2():
+    folder_name = 'boundingBO'
+    result_folder = 'testScribble'
+    files = os.listdir(folder_name)
+    for file in files:
+        if file == '.DS_Store':
+            continue
+
+        content = np.load(folder_name + "/" + file)
+
+        # print(file)
+        patientId = file.split("_")[0]
+        newName = file.split("_")[1].split(".")[0] + "." + file.split("_")[1].split(".")[2]
+        # os.rename(os.path.join(folder_name + '/' + file), newName)
+
+        # print(os.path.join(folder_name + '/' + file))
+        # print(newName)
+        np.save(result_folder + "/" + patientId + "/" + newName, content)
+
+
 #
-#         content = np.load(folder_name + "/" + file)
-#
-#         # print(file)
-#         patientId = file.split("_")[0]
-#         newName = file.split("_")[1].split(".")[0] + "." + file.split("_")[1].split(".")[2]
-#         # os.rename(os.path.join(folder_name + '/' + file), newName)
-#
-#         # print(os.path.join(folder_name + '/' + file))
-#         # print(newName)
-#         np.save(result_folder + "/" + patientId + "/" + newName, content)
-#
-#
-# def main3():
-#     testPath = "testScribble/210/IM-0001-0001.png.npy"
-#     testContent = np.load(testPath)
-#     print(testContent.shape)
-#
+def main3():
+    testPath = "testScribble/9/IM-0001-0002.npy"
+    testContent = np.load(testPath)
+    print(testContent.shape)
+
+
 #
 if __name__ == '__main__':
     # print('hello')
-    main()
+    main3()
